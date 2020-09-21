39c1ed69137fa69c22b7d7a1799d30d9198835e6
Ke <tyrantviru@gmail.com>
2020-09-21 10:09:44 +1000

    added aws sysops

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,149 +1,149 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
+    "imagebaseurl": "https://rbhatia46.github.io/",
     "name": "Ke(Eric) Yu",
     "role": "Software Engineer",
-    "linkedinId":"ke-yu-0ab14116b",
+    "linkedinId": "ke-yu-0ab14116b",
     "skypeid": "",
     "roleDescription": "I am passionate for learning new technologies and trying out interesting IoT & SmartHome project in my spare time.\nTraveling is one of my hobbies, the background is taken in Seoul, Koera",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/ke-yu-0ab14116b/",
-          "className":"fa fa-linkedin"
+    "socialLinks": [{
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/ke-yu-0ab14116b/",
+            "className": "fa fa-linkedin"
         },
         {
-          "name":"github",
-          "url":"https://github.com/rmit-s3686930-keyu",
-          "className":"fa fa-github"
+            "name": "github",
+            "url": "https://github.com/rmit-s3686930-keyu",
+            "className": "fa fa-github"
         },
-      ],
-    "aboutme":"My skills include quickly learning new technologies and programming languages, problem solving, cloud solution architecture, project management, object-oriented programming and MVC patterns. Currently i have exposure to programming languages Java, Kotlin, C/C#, Go, JS and Bootstrap. I've also got some hands-on experience with frameworks like Springboot, Angular.js, Vue.js. As for database technologies, i have worked with PostgreSQL and Oracle SQL. I'm still actively learning Python and .NET technologies. I am more than happy to learn new technologies to expand my tech stacks ",
-    "address":"Melbourne, Australia / Shanghai, China",
-    "website":"https://github.com/rmit-s3686930-keyu",
-    "education":[
-      {
-        "UniversityName":"Royal Melbourne Institute of Technology",
-        "specialization":"Master of Information Technology",
-        "MonthOfPassing":"Dec",
-        "YearOfPassing":"2019",
-        "Achievements":"Graduate with distinction"
-      },
-      {
-        "UniversityName":"Shanghai Dianji University",
-        "specialization":"Bachelor of Electrical Engineering",
-        "MonthOfPassing":"Sep",
-        "YearOfPassing":"2017",
-        "Achievements":""
-      }
     ],
-    "work":[
-      {
-        "CompanyName":"HospoHire",
-        "specialization":"Software Engineer Internship",
-        "MonthOfLeaving":"Dec",
-        "YearOfLeaving":"2019",
-        "Achievements":"Worked as a scrum master in an agile team"
-      },
-      {
-        "CompanyName":"OneBest Education",
-        "specialization":"Teaching Assistant",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Efficiently liaised between students, parents and teachers"
-      },
-      {
-        "CompanyName":"Shanghai Tobacco Group Corp.",
-        "specialization":"Assistant Electrical Engineer",
-        "MonthOfLeaving":"Jun",
-        "YearOfLeaving":"2017",
-        "Achievements":"Actively participated in the development of new models with senior engineers in a timely manner"
-      }
+    "aboutme": "My skills include quickly learning new technologies and programming languages, problem solving, cloud solution architecture, project management, object-oriented programming and MVC patterns. Currently i have exposure to programming languages Java, Kotlin, C/C#, Go, JS and Bootstrap. I've also got some hands-on experience with frameworks like Springboot, Angular.js, Vue.js. As for database technologies, i have worked with PostgreSQL and Oracle SQL. I'm still actively learning Python and .NET technologies. I am more than happy to learn new technologies to expand my tech stacks ",
+    "address": "Melbourne, Australia / Shanghai, China",
+    "website": "https://github.com/rmit-s3686930-keyu",
+    "education": [{
+            "UniversityName": "Royal Melbourne Institute of Technology",
+            "specialization": "Master of Information Technology",
+            "MonthOfPassing": "Dec",
+            "YearOfPassing": "2019",
+            "Achievements": "Graduate with distinction"
+        },
+        {
+            "UniversityName": "Shanghai Dianji University",
+            "specialization": "Bachelor of Electrical Engineering",
+            "MonthOfPassing": "Sep",
+            "YearOfPassing": "2017",
+            "Achievements": ""
+        }
     ],
-    "skillsDescription":"Technical Skills",
-    "skills":[
-      {
-        "skillname":"Java",
-        "skillpoints":80
-      },
-      {
-        "skillname":"Object-Oriented Programming",
-        "skillpoints":70
-      },
-      {
-        "skillname":"Agile Methodologies",
-        "skillpoints":50
-      },
-      {
-        "skillname":"AWS Cloud",
-        "skillpoints":50
-      },
-      {
-        "skillname":"Reactjs",
-        "skillpoints":20
-      }
+    "work": [{
+            "CompanyName": "HospoHire",
+            "specialization": "Software Engineer Internship",
+            "MonthOfLeaving": "Dec",
+            "YearOfLeaving": "2019",
+            "Achievements": "Worked as a scrum master in an agile team"
+        },
+        {
+            "CompanyName": "OneBest Education",
+            "specialization": "Teaching Assistant",
+            "MonthOfLeaving": "Jan",
+            "YearOfLeaving": "2018",
+            "Achievements": "Efficiently liaised between students, parents and teachers"
+        },
+        {
+            "CompanyName": "Shanghai Tobacco Group Corp.",
+            "specialization": "Assistant Electrical Engineer",
+            "MonthOfLeaving": "Jun",
+            "YearOfLeaving": "2017",
+            "Achievements": "Actively participated in the development of new models with senior engineers in a timely manner"
+        }
     ],
-    "portfolio":[
-      {
-        "name":"Hackathon",
-        "description":"RMIT 2019 Hackathon",
-        "imgurl":"./images/hackathon.jpg"
-      },
-      {
-        "name":"Snake",
-        "description":"Snake game on arduino controlled by a sony remote",
-        "thumbnail":"images/portfolio/snake.jpg",
-        "imgurl":"images/portfolio/snake.gif",
-        "projectUrl":"https://github.com/rmit-s3686930-keyu/Arduino_IRsnake"
-      },
-      {
-        "name":"AZ-900",
-        "description":"Microsoft Azure Fundamentals",
-        "imgurl":"images/portfolio/af.png",
-        "projectUrl":"https://www.youracclaim.com/earner/earned/badge/53be8507-e5bc-452c-897a-26ab35498362"
-      },
-      
-      {
-        "name":"OCAJP",
-        "description":"Oracle Certified Professional, Java SE 8 Programmer I (OCP)",  
-        "imgurl":"images/portfolio/ocajp.jpg",
-        "projectUrl":"https://www.youracclaim.com/badges/aeeff5f5-ca12-4892-bf73-8139089aa666/linked_in_profile"
-      },
-      {
-        "name":"Motion Controlled Lighting",
-        "description":"Turn on/off, change the hue/brightness with a motion sensor and rasspberry pi",
-        "thumbnail":"images/portfolio/rpi.png",
-        "imgurl":"images/portfolio/rpi.gif",
-        "projectUrl":"https://github.com/rmit-s3686930-keyu/rpi_pir_sensor"
-      },
-      {
-        "name":"AWS SAA",
-        "description":"AWS Certified Solutions Architect – Associate Certification",  
-        "imgurl":"images/portfolio/saa.png",
-        "projectUrl":"https://www.youracclaim.com/badges/9c68b588-b89d-44c8-ae0b-0007fe78f741?source=linked_in_profile"
-      },
-      {
-        "name":"AWS CPP",
-        "description":"AWS Certified Cloud Practitioner",
-        "imgurl":"images/portfolio/ccp.png",
-        "projectUrl":"https://www.youracclaim.com/badges/b814ea0c-013e-42da-a5be-e41f6c19c236/linked_in_profile"
-      },
-      {
-        "name":"CCNA",
-        "description":"Cisco Certified Network Associate Routing & Switching",
-        "imgurl":"images/portfolio/ccna.png",
-        "projectUrl":"https://www.youracclaim.com/badges/3b2d22b5-03f4-4244-bcb8-be9e9fb18224?source=linked_in_profile"
-      }
+    "skillsDescription": "Technical Skills",
+    "skills": [{
+            "skillname": "Java",
+            "skillpoints": 80
+        },
+        {
+            "skillname": "Object-Oriented Programming",
+            "skillpoints": 70
+        },
+        {
+            "skillname": "Agile Methodologies",
+            "skillpoints": 50
+        },
+        {
+            "skillname": "AWS Cloud",
+            "skillpoints": 50
+        },
+        {
+            "skillname": "Reactjs",
+            "skillpoints": 20
+        }
     ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
+    "portfolio": [{
+            "name": "Hackathon",
+            "description": "RMIT 2019 Hackathon",
+            "imgurl": "./images/hackathon.jpg"
+        },
+        {
+            "name": "Snake",
+            "description": "Snake game on arduino controlled by a sony remote",
+            "thumbnail": "images/portfolio/snake.jpg",
+            "imgurl": "images/portfolio/snake.gif",
+            "projectUrl": "https://github.com/rmit-s3686930-keyu/Arduino_IRsnake"
+        },
+        {
+            "name": "AZ-900",
+            "description": "Microsoft Azure Fundamentals",
+            "imgurl": "images/portfolio/af.png",
+            "projectUrl": "https://www.youracclaim.com/earner/earned/badge/53be8507-e5bc-452c-897a-26ab35498362"
+        },
+
+        {
+            "name": "OCAJP",
+            "description": "Oracle Certified Professional, Java SE 8 Programmer I (OCP)",
+            "imgurl": "images/portfolio/ocajp.jpg",
+            "projectUrl": "https://www.youracclaim.com/badges/aeeff5f5-ca12-4892-bf73-8139089aa666/linked_in_profile"
+        },
+        {
+            "name": "Motion Controlled Lighting",
+            "description": "Turn on/off, change the hue/brightness with a motion sensor and rasspberry pi",
+            "thumbnail": "images/portfolio/rpi.png",
+            "imgurl": "images/portfolio/rpi.gif",
+            "projectUrl": "https://github.com/rmit-s3686930-keyu/rpi_pir_sensor"
+        },
+        {
+            "name": "AWS SAA",
+            "description": "AWS Certified Solutions Architect – Associate Certification",
+            "imgurl": "images/portfolio/saa.png",
+            "projectUrl": "https://www.youracclaim.com/badges/9c68b588-b89d-44c8-ae0b-0007fe78f741?source=linked_in_profile"
+        },
+        {
+            "name": "AWS CPP",
+            "description": "AWS Certified Cloud Practitioner",
+            "imgurl": "images/portfolio/ccp.png",
+            "projectUrl": "https://www.youracclaim.com/badges/b814ea0c-013e-42da-a5be-e41f6c19c236/linked_in_profile"
+        },
+        {
+            "name": "AWS SYSOPS",
+            "description": "Certified SysOps Administrator - Associate",
+            "imgurl": "images/portfolio/sysops.png",
+            "projectUrl": "https://www.youracclaim.com/badges/b814ea0c-013e-42da-a5be-e41f6c19c236/linked_in_profile"
+        },
+        {
+            "name": "CCNA",
+            "description": "Cisco Certified Network Associate Routing & Switching",
+            "imgurl": "images/portfolio/ccna.png",
+            "projectUrl": "https://www.youracclaim.com/badges/3b2d22b5-03f4-4244-bcb8-be9e9fb18224?source=linked_in_profile"
+        }
+    ],
+    "testimonials": [{
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        },
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        }
     ]
-  }
-  
-  export default resumeData
+}
+
+export default resumeData
